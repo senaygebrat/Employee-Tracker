93ba7582a79408fa528b7a8dd1a2c42756909aab
Senay Gebrat <senaygebrat@gmail.com>
2022-10-30 18:21:33 -0700

    updated schema file for foreign keys

--- db/schema.sql
+++ db/schema.sql
@@ -5,21 +5,28 @@ CREATE DATABASE job_db;
 USE job_db;
 
 CREATE TABLE department (
-  id INT PRIMARY KEY,
+  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
   name VARCHAR(30)
 );
 
 CREATE TABLE role (
-  id INT PRIMARY KEY,
+  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
   title VARCHAR(30),
   salary DECIMAL,
-  department_id INT
+  department_id INT NOT NULL,
+  FOREIGN KEY (department_id)
+  REFERENCES department(id)
+  -- ON DELETE SET NULL
 );
 
 CREATE TABLE employee (
-  id INT PRIMARY KEY,
+  id INT PRIMARY KEY AUTO_INCREMENT,
   first_name VARCHAR(30),
   last_name VARCHAR(30),
-  role_id INT,
-  manager_id INT
-);
+  role_id INT NOT NULL,
+  manager_id INT NOT NULL,
+  FOREIGN KEY (role_id)
+  REFERENCES role(id),
+  FOREIGN KEY (manager_id)
+  REFERENCES employee(id)
+  );
--- db/seeds.sql
+++ db/seeds.sql
@@ -4,23 +4,23 @@ VALUES      (001, 'Engineering'),
             (003, 'Legal'),
             (004, 'Sales');
 
-INSERT INTO role (id, title, salary, department_id)
-VALUES      (010, 'Software Engineer', 120000, 001),
-            (012, 'Accountant', 125000, 002),
-            (014, 'Lawyer', 190000, 003),
-            (016, 'Legal Team Lead', 250000, 003),
-            (018, 'Sales Lead', 100000, 004),
-            (020, 'Lead Engineer', 150000, 001),
-            (022, 'Account Manager', 160000, 002),
-            (024, 'Salesman', 80000, 004);
+INSERT INTO role (title, salary, department_id)
+VALUES      ('Software Engineer', 120000, 001),
+            ('Accountant', 125000, 002),
+            ('Lawyer', 190000, 003),
+            ('Legal Team Lead', 250000, 003),
+            ('Sales Lead', 100000, 004),
+            ('Lead Engineer', 150000, 001),
+            ('Account Manager', 160000, 002),
+            ('Salesman', 80000, 004);
 
 
-INSERT INTO employee (id, first_name, last_name, role_id, manager_id)
-VALUES      (21, 'Senay', 'Gebrat', 010, 020),
-            (32, 'Bugs', 'Bunny', 012, 022),
-            (68, 'Daffy', 'Duck', 014, 016),
-            (47, 'Snow', 'White', 016, 018),
-            (12, 'Mickey', 'Mouse', 018, null),
-            (23, 'Donald', 'Duck', 020, null), 
-            (17, 'Minnie', 'Mouse', 022, null), 
-            (44, 'Lola', 'Bunny', 024, null); 
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES      ('Senay', 'Gebrat', 010, 020),
+            ('Bugs', 'Bunny', 012, 022),
+            ('Daffy', 'Duck', 014, 016),
+            ('Snow', 'White', 016, 018),
+            ('Mickey', 'Mouse', 018, null),
+            ('Donald', 'Duck', 020, null), 
+            ('Minnie', 'Mouse', 022, null), 
+            ('Lola', 'Bunny', 024, null); 
